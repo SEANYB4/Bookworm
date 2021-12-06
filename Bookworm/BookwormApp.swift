ba44c71ae4ac106ba6623b488649c79d9362e2ed
Sean Bain <75031999+SEANYB4@users.noreply.github.com>
2021-12-06 07:50:56 +0000

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Sean Bain on 06/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
